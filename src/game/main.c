40b3699907dd67c0f2d6b58f4eb186925633f052
Denzi <denis.weigel@icloud.com>
2026-07-08 10:53:06 +0200

    matched DrawWorld and DrawFade function

--- src/game/main.c
+++ src/game/main.c
@@ -1,6 +1,5 @@
 /*
  * Unit: game/main
- *
  * Functions:
  *   0x001c5490 SetTexAnimSignals
  *   0x001c5620 TextureCreate
@@ -43,3 +42,87 @@
  *   0x001cb568 BGLoadSfxFn
  *   0x001cb600 SetLevel
  */
+
+typedef signed int s32;
+
+struct numtx_s {
+    float _00, _01, _02, _03;
+    float _10, _11, _12, _13;
+    float _20, _21, _22, _23;
+    float _30, _31, _32, _33;
+}; /* 0x40 */
+
+struct nugscn_s;
+struct numtl_s;
+
+/* NuCamera: view matrix at 0x00 (_32 = 0x38 is the atmospheric-pressure Z). */
+struct nucam_s {
+    struct numtx_s mtx;
+};
+
+/* GameCam: the world camera matrix lives at 0x00. */
+struct gamecam_s {
+    struct numtx_s m;
+};
+
+extern s32 Level;
+extern s32 level_part_2;
+extern float AtmosphericPressureHackedZ;
+extern struct nugscn_s *world_scene[32];
+extern struct nucam_s *pNuCam;
+extern struct gamecam_s *pCam;
+extern struct numtl_s *DebMat[8];
+
+extern s32 fadeval;
+extern s32 SWIDTH;
+extern s32 SHEIGHT;
+extern s32 D_00633264;              /* fade colour */
+extern struct numtl_s *D_00633260; /* fade material */
+
+extern void NuCameraSet(struct nucam_s *cam);
+extern void NuGScnRndr3(struct nugscn_s *scene);
+extern void NuBridgeDraw(struct nugscn_s *scene, struct numtl_s *mtl);
+extern void NuWindDraw(struct nugscn_s *scene);
+extern void edobjRenderObjects(struct nugscn_s *scene);
+extern void NuRndrRect2di(s32 x, s32 y, s32 w, s32 h, s32 colour,
+                          struct numtl_s *mtl);
+
+void DrawWorld(void)
+{
+    if (Level == 0x18) {
+        if (world_scene[0] != 0) {
+            pNuCam->mtx._32 = AtmosphericPressureHackedZ;
+            NuCameraSet(pNuCam);
+            NuGScnRndr3(world_scene[0]);
+            NuBridgeDraw(world_scene[0], DebMat[6]);
+            NuWindDraw(world_scene[0]);
+            edobjRenderObjects(world_scene[0]);
+            pNuCam->mtx = pCam->m;
+            NuCameraSet(pNuCam);
+        }
+    } else if (level_part_2 != 0) {
+        if (world_scene[1] != 0) {
+            NuGScnRndr3(world_scene[1]);
+            NuBridgeDraw(world_scene[1], DebMat[6]);
+            NuWindDraw(world_scene[1]);
+            edobjRenderObjects(world_scene[1]);
+        }
+    } else {
+        if (world_scene[0] != 0) {
+            NuGScnRndr3(world_scene[0]);
+            NuBridgeDraw(world_scene[0], DebMat[6]);
+            NuWindDraw(world_scene[0]);
+            edobjRenderObjects(world_scene[0]);
+        }
+    }
+}
+
+void DrawFade(void)
+{
+    if (fadeval != 0) {
+        NuRndrRect2di(0, 0, SWIDTH << 4, SHEIGHT << 3, D_00633264,
+                      D_00633260);
+    }
+}
+
+
