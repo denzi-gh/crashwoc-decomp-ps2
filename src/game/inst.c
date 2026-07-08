e976504b240968cab94084a05a82c15906ae72bf
Denzi <146925354+denzi-gh@users.noreply.github.com>
2026-07-08 16:08:52 +0200

    Decompile InstAnimDataLoad candidate

--- src/game/inst.c
+++ src/game/inst.c
@@ -12,9 +12,11 @@
  *   0x001d5f38 InstInit
  */
 
+union variptr_u;
 struct nulsthdr_s;
 struct nulnkhdr_s;
 struct nuscene_s;
+struct nuanimdata_s;
 
 struct sceneinst_s {
     struct nuscene_s *scene;
@@ -22,22 +24,27 @@ struct sceneinst_s {
     int inst_cnt;
 };
 
-extern struct nulsthdr_s *D_006309FC;
-extern struct nulsthdr_s *D_00630A00;
+struct animdatainst_s {
+    struct nuanimdata_s *ad;
+    char name[0x100];
+    int inst_cnt;
+};
 
-#define sceneinst_pool D_006309FC
-#define animdatainst_pool D_00630A00
 extern struct nulsthdr_s *D_006309fc;
 extern struct nulsthdr_s *D_00630a00;
 
 #define sceneinst_pool D_006309fc
 #define animdatainst_pool D_00630a00
 
+extern union variptr_u superbuffer_ptr;
+extern union variptr_u superbuffer_end;
+
 extern struct nulnkhdr_s *NuLstGetNext(struct nulsthdr_s *hdr, struct nulnkhdr_s *lnk);
 extern struct nulnkhdr_s *NuLstAlloc(struct nulsthdr_s *hdr);
 extern void NuLstFree(struct nulnkhdr_s *lnk);
 extern struct nuscene_s *NuSceneLoad(char *name);
 extern void NuSceneDestroy(struct nuscene_s *scene);
+extern struct nuanimdata_s *NuAnimDataLoadBuff(char *name, union variptr_u *buff, union variptr_u *endbuff);
 extern int strcasecmp(const char *s1, const char *s2);
 extern char *strcpy(char *dest, const char *src);
 
@@ -82,4 +89,33 @@ void InstSceneDestroy(struct nuscene_s *scene) {
         }
         sc = (struct sceneinst_s *)NuLstGetNext(sceneinst_pool, (struct nulnkhdr_s *)sc);
     }
+}
+
+struct nuanimdata_s *InstAnimDataLoad(char *name) {
+    struct animdatainst_s *lst;
+    struct nuanimdata_s *adat;
+    struct animdatainst_s *sc;
+
+    sc = (struct animdatainst_s *)NuLstGetNext(animdatainst_pool, 0);
+    while (sc != 0) {
+        if (strcasecmp(name, sc->name) == 0) {
+            sc->inst_cnt++;
+            return sc->ad;
+        }
+        sc = (struct animdatainst_s *)NuLstGetNext(animdatainst_pool, (struct nulnkhdr_s *)sc);
+    }
+
+    lst = (struct animdatainst_s *)NuLstAlloc(animdatainst_pool);
+    if (lst != 0) {
+        adat = NuAnimDataLoadBuff(name, &superbuffer_ptr, &superbuffer_end);
+        lst->ad = adat;
+        if (adat != 0) {
+            strcpy(lst->name, name);
+            lst->inst_cnt = 1;
+            return lst->ad;
+        }
+        NuLstFree((struct nulnkhdr_s *)lst);
+    }
+
+    return 0;
 }
