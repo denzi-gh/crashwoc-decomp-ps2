125316745c9503de2f09da230b40193b77dc8224
Denzi <denis.weigel@icloud.com>
2026-07-14 07:39:09 +0200

    game/cloudfx: match CloseClouds, cloudProcess, TimeTunnelClose, CloudFxInit

--- src/game/cloudfx.c
+++ src/game/cloudfx.c
@@ -13,3 +13,40 @@
  *   0x0024f2b0 TimeTunnelClose
  *   0x0024f2e0 CloudFxInit
  */
+
+#include "creature.h"
+
+extern s32 cloudreset;
+extern void *ttmtl;
+extern void *tttex;
+extern void *clouds_rs;
+extern u8 D_00293090[];
+
+extern void cloudInit();
+extern void NuMtlDestroy(void *mtl);
+extern void *NuRndrStreamAllocStream(void *a0, void *a1, s32 a2);
+
+
+void cloudProcess(void) {
+    if (cloudreset != 0) {
+        cloudInit();
+        cloudreset = 0;
+    }
+}
+
+void CloseClouds(void) {
+}
+
+void TimeTunnelClose(void) {
+    if (ttmtl != 0) {
+        NuMtlDestroy(ttmtl);
+    }
+    ttmtl = 0;
+    tttex = 0;
+}
+
+void CloudFxInit(void) {
+    if (clouds_rs == 0) {
+        clouds_rs = NuRndrStreamAllocStream((void *)0x10000000, D_00293090, 0x3C);
+    }
+}
