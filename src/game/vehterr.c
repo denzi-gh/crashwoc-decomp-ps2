5e8c1271cf267751050e6bf05c8dd9eff74e3d31
Denzi <denis.weigel@icloud.com>
2026-07-14 07:00:34 +0200

    game/vehterr: match FindSurfaceRotXZFromNormal (GC, tweaked)
    u16 rx (andi, no short-cast); NuAtan2D/NuVecRotateX take int (no arg
    truncation); binding 2nd NuAtan2D result to a temp schedules *rotX store
    after the call as retail does. nuvec_s from creature.h.

--- src/game/vehterr.c
+++ src/game/vehterr.c
@@ -12,3 +12,21 @@
  *   0x00223948 FindVehicleNormalGiven4Points
  *   0x00223a70 FindSurfaceRotXZFromNormal
  */
+
+#include "creature.h"
+
+extern int NuAtan2D(float a, float b);
+extern void NuVecRotateX(struct nuvec_s *dst, struct nuvec_s *src, int angle);
+
+
+void FindSurfaceRotXZFromNormal(struct nuvec_s *normal, s16 *rotX, s16 *rotZ) {
+    struct nuvec_s temp;
+    u16 rx;
+    int rz;
+
+    rx = NuAtan2D(normal->z, normal->y);
+    NuVecRotateX(&temp, normal, -rx);
+    rz = NuAtan2D(temp.x, temp.y);
+    *rotX = rx;
+    *rotZ = -rz;
+}
