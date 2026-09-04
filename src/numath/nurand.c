5e8f71e2c43c9581681078b3abdf2638ef8928e9
Open-Travellers <58868054@users.noreply.github.com>
2026-09-04 18:17:30 +0200

    Update nurand.c

--- src/numath/nurand.c
+++ src/numath/nurand.c
@@ -30,40 +30,6 @@ void NuRandSetSeed(struct nunrand_s *nrand,int seed) {
   temp->idum = seed;
 }
 
-long int __divdi3 (long int u, long int v);
-
-long NuRand(struct nunrand_s* nrand) {
-    struct nunrand_s* temp;
-    long temp_s0;
-    long temp_v0;
-
-    temp = (nrand != 0) ? nrand : &global_rand;
-    temp_s0 = temp->idum ^ 0x075BD924;
-    temp->idum = temp_s0;
-    temp_v0 = __divdi3(temp_s0, 0x31E5);
-    temp->idum = ((temp_s0 - (temp_v0 * 0x31E5)) * 0x41A7) - (temp_v0 * 0xB14);
-    if (temp->idum < 0) {
-        temp->idum = (long) (temp->idum + 0x7FFFFFFF);
-    }
-    temp->idum = (long) (temp->idum ^ 0x075BD924);
-    return temp->idum;
-}
-
-f32 NuFloatRand(struct nunrand_s * nrand) {
-    long temp_v0;
-    struct nunrand_s* temp;
-
-    temp = (nrand != 0) ? nrand : &global_rand;
-    temp->idum = temp->idum ^ 0x075BD924;
-    temp_v0 = __divdi3(temp->idum, 0x31E5);
-    temp->idum = ((temp->idum - (temp_v0 * 0x31E5)) * 0x41A7) - (temp_v0 * 0xB14);
-    if (temp->idum < 0) {
-        temp->idum = temp->idum + 0x7FFFFFFF;
-    }
-    temp->idum = temp->idum ^ 0x075BD924;
-    return __floatdisf((int) temp->idum) * 4.656613e-10f;
-}
-
 float NuRandFloat(void) {
   fseed = (fseed * 0x19660d) + 0x3C6EF35F;
   return (float)((int)(fseed & 0x7fffff) | 0x3f800000) - 1.0f;
