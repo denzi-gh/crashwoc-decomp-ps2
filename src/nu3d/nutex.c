97f41f5268b9afe7eeb8633532d9382e539f892e
Open-Travellers <58868054@users.noreply.github.com>
2026-09-08 02:37:22 +0200

    update nutex and nufpar

--- src/nu3d/nutex.c
+++ src/nu3d/nutex.c
@@ -72,6 +72,86 @@ typedef struct NuTex {
     u8 unk0D0[0x10];
 } NuTex;
 
+struct ps2tex_s { // 0xb0
+	/* 0x00 */ struct ps2tex_s *succ;
+	/* 0x04 */ struct ps2tex_s *prev;
+	/* 0x08 */ short unsigned int lowaddr;
+	/* 0x0a */ short unsigned int highaddr;
+	/* 0x0c */ short unsigned int req;
+	/* 0x0e:0 */ short unsigned int housed : 1;
+	/* 0x10 */ short unsigned int p2w;
+	/* 0x12 */ short unsigned int p2h;
+	/* 0x14 */ struct _sceDmaTag *dt_load[7];
+	/* 0x30 */ unsigned int loadsize[7];
+	/* 0x4c */ unsigned int offset[7];
+	/* 0x68 */ short unsigned int loadaddr[7];
+	/* 0x76 */ short unsigned int loadwidth[7];
+	/* 0x84 */ short unsigned int bltwidth[7];
+	/* 0x92 */ unsigned char nummm;
+	/* 0x93 */ unsigned char psm;
+	/* 0x94 */ unsigned char bltpsm;
+	/* 0x95 */ unsigned char tcc;
+	/* 0x96 */ unsigned char texa;
+	/* 0x97 */ unsigned char tfx;
+	/* 0x98 */ struct sceDmaTag *clut_load;
+	/* 0x9c */ struct sceDmaTag *clut_alt;
+	/* 0xa0 */ short unsigned int clut_addr;
+	/* 0xa2 */ short unsigned int clut_width;
+	/* 0xa4 */ short unsigned int clut_psm;
+	/* 0xa6 */ short unsigned int clut_offset;
+	/* 0xa8 */ short unsigned int clut_loadsize;
+	/* 0xac */ unsigned int tmpaddr;
+};
+
+enum nutextype_e
+{
+    NUTEX_NUMTEXTURETYPES = 26,
+    NUTEX_RZT16 = 25,
+    NUTEX_RZT24 = 24,
+    NUTEX_RZT32 = 23,
+    NUTEX_RTT16 = 22,
+    NUTEX_RTT24 = 21,
+    NUTEX_RTT32 = 20,
+    NUTEX_RT16 = 19,
+    NUTEX_RT24 = 18,
+    NUTEX_RT32 = 17,
+    NUTEX_RT8H = 16,
+    NUTEX_RTT8 = 15,
+    NUTEX_RT8 = 14,
+    NUTEX_BB = 13,
+    NUTEX_DXT5 = 12,
+    NUTEX_DXT4 = 11,
+    NUTEX_DXT3 = 10,
+    NUTEX_DXT2 = 9,
+    NUTEX_DXT1 = 8,
+    NUTEX_PAL8_S = 7,
+    NUTEX_PAL4_S = 6,
+    NUTEX_PAL8 = 5,
+    NUTEX_PAL4 = 4,
+    NUTEX_RGBA32 = 3,
+    NUTEX_RGB24 = 2,
+    NUTEX_RGBA16 = 1,
+    NUTEX_RGB16 = 0
+};
+
+struct nutex_s { // 0x1c
+	/* 0x00 */ enum nutextype_e type;
+	/* 0x04 */ int width;
+	/* 0x08 */ int height;
+	/* 0x0c */ int mmcnt;
+	/* 0x10 */ void *bits;
+	/* 0x14 */ int *pal;
+};
+
+struct nusystex_s { // 0xe0
+	/* 0x00 */ struct nutex_s tex;
+	/* 0x18 */ long ref;
+	/* 0x20 */ struct ps2tex_s ps2tex;
+	/* 0x1a:0 */ short int used;
+	/* 0x1a:1 */ short int xdef;
+    int pad[3];
+};
+
 extern NuTex *D_0062EBEC;
 #define TexList D_0062EBEC
 
@@ -84,6 +164,11 @@ extern int D_0062EC04;
 extern int D_0067D640[256];
 #define gs_reserve_stack D_0067D640
 
+extern int nutex_alloc_cnt;
+extern int tex_allocmax;
+extern int tpid;
+extern struct nusystex_s * tinfo;
+
 extern int NuTexReadBitmapMMEx(char *name, int mipmaps, int *addr, int *addrend);
 extern int NuTexCreateEx(NuTex *spec, void *extmem, int extsize);
 extern int NuTexRecoverNativeDataEx(int tex, void *dest, int size, int header, int image, int palette);
@@ -95,6 +180,26 @@ int NuTexGetSysBuffSize(int count) {
     return count * sizeof(NuTex);
 }
 
+int NuTexReserve(int size)
+{
+    int rounded;
+    int aligned;
+
+    if (gs_reserve_sp >= 0x100)
+        return 0;
+
+    rounded = size + 0xff;
+    if (rounded < 0)
+        rounded += 0xff;
+
+    gs_reserve_stack[gs_reserve_sp] = gs_botfree;
+    gs_reserve_sp++;
+
+    aligned = (gs_botfree + 0x1f) & 0xffffffe0;
+    gs_botfree = aligned + (rounded >> 8);
+
+    return aligned;
+}
 
 void NuTexUnReserve(void) {
     if (gs_reserve_sp) {
@@ -116,6 +221,24 @@ int NuTexReadBitmapMM(char *name, int mipmaps) {
 
 int NuTexCreate(NuTex *spec) {
     return NuTexCreateEx(spec, 0, 0);
+}
+
+int NuPs2TxDestroy(struct ps2tex_s * tex);
+void NuMemFreeFn(void * addr, char * file, int line);
+
+void NuTexDestroy(int tid) {
+  tid--;
+  if (tid >= 0) {
+    if ((tinfo[tid].ref & 0x20000U) == 0) {
+      if (tinfo[tid].tex.bits != 0) {
+        NuMemFreeFn(tinfo[tid].tex.bits,"..\\nu2.ps2\\nu3d\\nutex.c",0x3f6);
+        tinfo[tid].tex.bits = 0;
+      }
+      NuPs2TxDestroy(&tinfo[tid].ps2tex);
+    }
+    tinfo[tid].ref = tinfo[tid].ref & 0xfffffffffffeffff;
+    nutex_alloc_cnt--;
+  }
 }
 
 
@@ -265,6 +388,17 @@ void NuTexPalChangeStreamTemp(int tex) {
     }
 }
 
+int Ps2TxGetTex0(struct ps2tex_s* ptex);
+
+int NuTexGetTex0(int tid, int palid) {
+    int id;
+
+    id = tid - 1;
+    if ((id >= 0) && ((tinfo[id].ref & 0x10000U) != 0)) {
+        return Ps2TxGetTex0(&tinfo[id].ps2tex);
+    }
+    return 0;
+}
 
 int NuTexGetTextAddr(int tex) {
     if (TexList[tex - 1].flags & 0x10000) {
